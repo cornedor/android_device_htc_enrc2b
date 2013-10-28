4bb8beb428a7e56cf32c0e13c61c16595f4d4529
Rob Shannon <spirit2457@gmail.com>
2013-10-28 14:19:28 +0000

    Change defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 
 # Try to build the kernel
 TARGET_KERNEL_SOURCE := kernel/htc/enrc2b
-TARGET_KERNEL_CONFIG := pap_defconfig
+TARGET_KERNEL_CONFIG := lenny_defconfig
 
 # dont build docs
 DISABLE_DROIDDOC := true
